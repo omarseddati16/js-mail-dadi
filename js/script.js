4ae846fe187324096a21d17622d9408eb495407a
omarseddati16 <omarseddati16@gmail.com>
2025-07-04 15:51:45 +0200

    dadi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Creare una lista di email 
 const emailinvites = ['sergio@gmail.com', 'luca@hotmail.com', 'paolo@yahoo.com', 'giacomo@hotmail.com', 'antonio@gmail.com']
-// Chiedere all'utente l'email
+// Chiedere all'utente l'email 
 let email = prompt("Inserisci la tua email");
 let find = false
 // Creazione ciclo 
@@ -13,4 +13,36 @@ if (find) {
   console.log('Invitato');
 } else {
   console.log('Non invitato')
+}
+
+// Dadi
+
+const computer = []
+const giocatore = []
+
+let punticomputer = 0;
+let puntigiocatore = 0;
+
+console.log(Math.floor(Math.random() * 6) + 1)
+
+for (let i = 1; i < 2; i++) {
+  const computer = (Math.floor(Math.random() * 6) + 1);
+  const giocatore = parseInt(prompt('Metti un numero'))
+
+  numbericomputer.push(computer);
+  numberigiocare.push(giocatore);
+
+  if (computer > giocatore) {
+    punticomputer++;
+  }
+  else if (computer < giocatore) {
+    puntigiocatore++;
+  }
+}
+
+if (punticomputer > puntigiocatore) {
+  console.log('pc numeri ${computer}. giocatore numeri ${giocatore}. Vince computer')
+}
+else if (punticomputer < puntigiocatore) {
+  console.log('pc numeri ${computer}. giocatore numeri ${giocatore}. Vince giocatore')
 }
