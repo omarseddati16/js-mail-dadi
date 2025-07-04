18214146156dd36a90ef476b06d7ebd0e6a36b5b
omarseddati16 <omarseddati16@gmail.com>
2025-07-04 14:37:05 +0200

    fix a [emailinvites] e fine mail

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Creare una lista di email 
-const emailinvites = ['Sergio', 'Luca', 'Paolo', 'Giacomo', 'Antonio']
+const emailinvites = ['sergio@gmail.com', 'luca@hotmail.com', 'paolo@yahoo.com', 'giacomo@hotmail.com', 'antonio@gmail.com']
 // Chiedere all'utente l'email
 let email = prompt("Inserisci la tua email");
+let find = false
+// Creazione ciclo 
+for (let i = 0; i < emailinvites.length; i++) {
+  if (emailinvites[i] === email) {
+    find = true;
+  }
+}
+if (find) {
+  console.log('Invitato');
+} else {
+  console.log('Non invitato')
+}
